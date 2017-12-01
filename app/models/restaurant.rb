661b9412cff83620738358b43b8dc792aee4e7e4
github username <siobhan.p.mahoney@gmail.com>
2017-12-01 11:32:49 -0500

    completed in solution.rb

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,34 @@
 class Restaurant
-  attr_accessor :name
+  attr_accessor :name, :customers
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.find do |obj|
+      obj.name == name
+    end
+  end
+
+  def reviews
+    Review.all.select do |obj|
+      obj.restaurant == self
+    end
+  end
+
+  def customers
+    self.reviews.map do |obj|
+      obj.customer
+    end
+  end
+
+
 
 end
--- solution.rb
+++ solution.rb
@@ -1,1 +1,93 @@
-# Please copy/paste all three classes into this file to submit your solution!
+class Customer
+  attr_accessor :first_name, :last_name
+  @@all = []
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name  = last_name
+    @@all << self
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    name_array = name.split
+    self.all.find do |customer_obj|
+      customer_obj.first_name == name_array[0] && customer_obj.last_name == name_array[1]
+    end
+  end
+
+  def self.find_all_by_first_name(first)
+    self.all.select do |customer_obj|
+      customer_obj.first_name == first
+    end
+  end
+
+  def self.all_names
+    self.all.map do |name_obj|
+      name_obj.full_name
+    end
+  end
+
+  def add_review(restaurant, content)
+    new_review = Review.new(restaurant, content)
+    new_review.customer = self
+  end
+
+end
+
+class Review
+  attr_accessor :restaurant, :content, :customer
+
+  @@all = []
+
+  def initialize(restaurant, content)
+    @restaurant = restaurant
+    @content = content
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
+
+
+class Restaurant
+  attr_accessor :name, :customers
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.find do |obj|
+      obj.name == name
+    end
+  end
+
+  def reviews
+    Review.all.select do |obj|
+      obj.restaurant == self
+    end
+  end
+
+  def customers
+    self.reviews.map do |obj|
+      obj.customer
+    end
+  end
+end
