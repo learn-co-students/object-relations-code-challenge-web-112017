c0af35f9651984a99bd359ab472760b98f502ed7
KellyStamps <kellydsample@gmail.com>
2017-12-01 11:15:14 -0500

    added all required code to restaurant.rb, review.rb, customer.rb, console.rb

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,53 @@
 class Customer
   attr_accessor :first_name, :last_name
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    #******DONE*********************************
+    @@all
+    # Customer.all
+    # should return all of the customers
+  end
+
+  def self.find_by_name(name)
+    #******DONE*********************************
+    name = name.split(" ")
+    @@all.find do |customer|
+      customer.first_name == name[0] && customer.last_name == name[1]
+    end
+    # Customer.find_by_name(name)
+    # given a string of a full name, returns the first customer whose full name matches
+  end
+
+  def self.find_all_by_first_name (name)
+    #******DONE*********************************
+    @@all.select { |customer| customer.first_name == name  }
+    # Customer.find_all_by_first_name(name)
+    # given a string of a first name, returns an array containing all customers with that first name
+  end
+
+  def self.all_names
+    #******DONE*********************************
+    @@all.collect {|customer| "#{customer.first_name} #{customer.last_name}" }
+    # Customer.all_names
+    # should return an array of all of the customer full names
+  end
+
+  def add_review (restaurant_instance, content)
+    #******DONE*********************************
+    Review.new(self,restaurant_instance, content)
+    # Customer#add_review(restaurant, content)
+    # given some content and a restaurant, creates a new review and associates it with that customer and that restaurant
+  end
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,34 @@
 class Review
-  
-end
+  attr_accessor :customer, :restaurant, :content
+  @@all = []
 
+  def initialize (customer_instance, restaurant_instance, content)
+    @customer = customer_instance
+    @restaurant = restaurant_instance
+    @content = content
+    @@all << self
+  end
+
+  def self.all
+    #*****************DONE***************
+    @@all
+    # Review.all
+    # returns all of the reviews
+  end
+
+  def customer
+    #*****************DONE***************
+    @customer
+    # Review#customer
+    # returns the customer for that given review
+  end
+
+
+  def restaurant
+    #*****************DONE***************
+    @restaurant
+    # Review#restaurant
+    # returns the restaurant for that given review
+  end
+
+end
