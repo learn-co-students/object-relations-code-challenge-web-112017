08071cc51ac0aa960abb5d60f3e070b80bfec7e2
eal6gf <elum1234@gmail.com>
2017-12-01 11:20:26 -0500

    Done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,37 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.find{|customer| customer.full_name == name}
+  end
+
+  def self.find_all_by_first_name(name)
+    self.all.select{|customer| customer.first_name == name}
+  end
+
+  def self.all_names
+    self.all.map do |customer|
+      customer.full_name
+    end
+  end
+
+  def add_review(restaurant, content)
+    new_review = Review.new(self, restaurant, content)
   end
 end
