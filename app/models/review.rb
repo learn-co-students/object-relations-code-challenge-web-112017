fe1e5c3a9763bc1cebb9e06353f78cc08bbadf4c
iamm28 <alisonmackay246@gmail.com>
2017-12-01 11:32:58 -0500

    done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,25 @@
 class Review
-  
-end
+  @@all = []
+  #attr_reader  #may need to be accessor
+  attr_accessor :customer, :restaurant, :content
+  def initialize(restaurant,content)
+    @restaurant = restaurant
+    @content = content
+    @@all << self
+    #@customer = nil
+  end
 
+  def self.all
+    @@all
+  end
+
+  #def customer
+    #returns the customer for that given review
+    #should be from attr_reader
+  #end
+
+  #def restaurant
+    #returns the restaurant for that given review
+    #should be from
+  #end
+end
