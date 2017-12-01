43253240e0a1a0509acfdfef66b74ac5c13ad689
Flatiron School <leslie.hsu@flatironschool.com>
2017-12-01 11:36:34 -0500

    Done.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,37 @@
 class Restaurant
   attr_accessor :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    Restaurant.all.find do |restaurant|
+      restaurant.name == name
+    end
+  end
+
+  def reviews
+    Review.all.select do |review|
+      review.restaurant == self
+    end
+  end
+
+  def customers
+    Review.all.select do |review|
+      review.restaurant == self
+    end.map do |review|
+      review.customer
+  end.uniq
+end
+
+
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,26 @@ def reload
   load 'config/environment.rb'
 end
 
-Pry.start
+leslie_hsu = Customer.new("Leslie", "Hsu")
+sebastian_royer = Customer.new("Sebastian", "Royer")
+leslie_jones = Customer.new("Leslie", "Jones")
+
+Customer.find_by_name("Leslie Hsu")
+Customer.find_all_by_first_name("Leslie")
+Customer.all_names
+
+tomoe = Restaurant.new("Tomoe")
+mamouns = Restaurant.new("Mamouns")
+tomoe2 = Restaurant.new("Tomoe")
+sakamai = Restaurant.new("Sakamai")
+
+leslie_hsu.add_review(tomoe, "Is a wonderful, homey sushi restaurant")
+leslie_hsu.add_review(mamouns, "delicious falafel")
+sebastian_royer.add_review(mamouns, "pretty good falafel")
+
+
+#stick instances above here
+
+binding.pry
+puts "done!"
+#write code here, in order to get to the pry (if not already happening)
