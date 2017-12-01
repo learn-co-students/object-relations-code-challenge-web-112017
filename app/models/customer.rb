4967ad9c38a65a5c0853b570d2d9086c4d2cf121
github username <github email address>
2017-12-01 11:33:44 -0500

    submission

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,49 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    #given a string of a full name, returns
+    #the first customer whose full name matches
+    search_name = name.split(' ')
+    Customer.all.each do |customer|
+      if search_name[0] == customer.first_name && search_name[1] == customer.last_name
+        return customer
+      end
+    end
+  end
+
+  def self.find_all_by_first_name(name)
+    Customer.all.select do |customer|
+      customer.first_name == name
+    end
+  end
+
+  def self.all_names
+    #should return an array of all of the customer full names
+    Customer.all.map do |customer|
+      "#{customer.first_name} #{customer.last_name}"
+    end
+  end
+
+  def add_review(restaurant, content)
+    #given some content and a restaurant,
+    #creates a new review and associates it with that customer and that restaurant
+    review = Review.new(restaurant, content, self)
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,49 @@
 class Restaurant
   attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    #given a string of restaurant name, returns the first restaurant that matches
+    Restaurant.all.select do |restaurant|
+      restaurant.name.downcase == name.downcase
+    end
+  end
+
+  def reviews
+    #returns an array of all reviews for that restaurant
+    myReviews = []
+    Review.all.each do |review|
+      if review.restaurant == self
+        myReviews << review
+      end
+    end
+    myReviews
+    # Review.all.select do |review|
+    #   review.restaurant == self
+    # end
+  end
+
+  def customers
+    #should return all of the customers who have written reviews of that restaurant.
+    myReviewCustomers = []
+    Review.all.each do |review|
+      if review.restaurant == self
+        myReviewCustomers << review.customer
+      end
+    end
+    myReviewCustomers
+    # Review.all.select do |review|
+    #   review.restaurant
+    # end
+  end
 end
