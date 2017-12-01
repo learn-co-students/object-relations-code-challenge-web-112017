c4f8d20fe0f3c22733006f07a461b274f473c480
Matt McAlister <matt.mcalister93@gmail.com>
2017-12-01 11:01:22 -0500

    finished building classes Restaurant, Customer, and Review

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,45 @@
 class Customer
+
+  @@all = []
+
   attr_accessor :first_name, :last_name
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+
+  def self.all
+  #   + should return all of the customers
+    @@all
+  end
+
+  def self.find_by_name(name)
+  #   + given a string of a full name, returns the first customer whose full name matches
+    self.all.find {|customer| customer.full_name == name}
+  end
+
+  def self.find_all_by_first_name(name)
+  #   + given a string of a first name, returns an array containing all customers with that first name
+    self.all.select {|customer| customer.first_name == name}
+  end
+
+
+  def self.all_names
+  #   + should return an array of all of the customer full names
+    self.all.map {|customer| customer.full_name}
+  end
+
+  def add_review(restaurant, content)
+  #   + given some content and a restaurant, creates a new review and
+  #    associates it with that customer and that restaurant
+    Review.new(self, restaurant, content)
+  end
+
 end
--- solution.rb
+++ solution.rb
@@ -1,1 +1,109 @@
 # Please copy/paste all three classes into this file to submit your solution!
+
+
+class Review
+
+  attr_accessor :customer, :restaurant, :content
+  # + Review#customer
+  #   + returns the customer for that given review
+  # + Review#restaurant
+  #   + returns the restaurant for that given review
+
+  @@all = []
+
+  def initialize(customer, restaurant, content)
+    @customer = customer
+    @restaurant = restaurant
+    @content = content
+    @@all << self
+  end
+
+  def self.all
+  #   + returns all of the reviews
+    @@all
+  end
+
+end
+
+
+
+class Restaurant
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+  #   + returns an array of all restaurants
+    @@all
+  end
+
+
+  def self.find_by_name(name)
+  #   + given a string of restaurant name, returns the first restaurant that matches
+    self.all.find {|restaurant| restaurant.name == name}
+  end
+
+  def reviews
+  #   + returns an array of all reviews for that restaurant
+    Review.all.select {|review| review.restaurant == self}
+  end
+
+  def customers
+  #   + should return all of the customers who have written reviews of that restaurant.
+    self.reviews.map {|review| review.customer}.uniq
+  end
+
+end
+
+
+
+class Customer
+
+  @@all = []
+
+  attr_accessor :first_name, :last_name
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name  = last_name
+    @@all << self
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+
+  def self.all
+  #   + should return all of the customers
+    @@all
+  end
+
+  def self.find_by_name(name)
+  #   + given a string of a full name, returns the first customer whose full name matches
+    self.all.find {|customer| customer.full_name == name}
+  end
+
+  def self.find_all_by_first_name(name)
+  #   + given a string of a first name, returns an array containing all customers with that first name
+    self.all.select {|customer| customer.first_name == name}
+  end
+
+
+  def self.all_names
+  #   + should return an array of all of the customer full names
+    self.all.map {|customer| customer.full_name}
+  end
+
+  def add_review(restaurant, content)
+  #   + given some content and a restaurant, creates a new review and
+  #    associates it with that customer and that restaurant
+    Review.new(self, restaurant, content)
+  end
+
+end
