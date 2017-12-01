0bcf0fa9d527b50fd64e23342a15b5e8927b6ff3
mattfaircloth <mattcfaircloth@gmail.com>
2017-12-01 11:33:26 -0500

    Done.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,32 @@
 class Restaurant
   attr_accessor :name
-
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def reviews
+    Review.all.select do |each_review|
+      each_review.restaurant == self
+    end
+  end
+
+  def customers
+    Review.all.select do |each_review|
+      each_review.restaurant == self
+     end.map do |filtered_review|
+        filtered_review.customer
+     end
+   end
+
+   def self.find_by_name(name)
+      self.all.select do |rest_array|
+         rest_array.name == name
+      end
+   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,26 @@ def reload
   load 'config/environment.rb'
 end
 
-Pry.start
+upland = Restaurant.new("Upland")
+gramercy_tavern = Restaurant.new("Gramercy Tavern")
+pizza_park = Restaurant.new("Pizza Park")
+crave= Restaurant.new("Crave Fish Bar")
+
+matt = Customer.new("Matt", "Faircloth")
+kyra = Customer.new("Kyra", "Rosow")
+johann = Customer.new("Johann", "Kerr")
+ryan = Customer.new("Ryan", "Bures")
+john = Customer.new("Ryan,", "Johnson")
+
+matt.add_review(upland, "This restaurant is great!")
+kyra.add_review(gramercy_tavern, "This has the best burger.")
+johann.add_review(pizza_park, "Pizza is so good!!")
+ryan.add_review(crave, "Freshest fish ever.")
+ryan.add_review(upland, "mmmmmmm")
+
+
+
+
+binding.pry
+
+hello = "Hello World"
